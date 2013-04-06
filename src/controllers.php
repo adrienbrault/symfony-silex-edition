bf9a296f744e25e9f321a02d86d590781648cddd
Adrien Brault <adrien.brault@gmail.com>
2013-04-07 01:36:17 +0200

    Easy templates and render a template

--- src/controllers.php
+++ src/controllers.php
@@ -3,3 +3,7 @@
 $kernel->get('/adrien', function () {
     return 'Hello!';
 });
+
+$kernel->get('/say/{message}', function ($message) use ($kernel) {
+    return $kernel['templating']->renderResponse('about.twig', array('message' => $message));
+});
